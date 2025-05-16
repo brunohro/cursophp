3f926d727c04a1a21c58470654de436a4dea0bcb
brunohro <brunoh.rodrigues27@gmail.com>
2025-05-16 18:19:58 -0300

    Variáveis e Constantes

---
+++ extra/variaveis_e_constantes.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Extra</title>
+</head>
+
+<body>
+    <h1>Variáveis e constantes</h1>
+    <?php 
+    $name = "Bruno Oliveira - ";
+    const IDADE = 19; // CONST 
+    echo "Meu nome é $name ";
+    echo "Agora vou atualizar para - ";
+    $name = "Henrique Oliveira";
+    echo "$name, e sua idade é " . IDADE;
+
+    // idade e uma constante 
+    // name e uma variavel
+    ?>
+
+
+</body>
+
+</html>
