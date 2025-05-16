390beb9be6b8182df0e8dc429067ed42fc18fc4d
brunohro <brunoh.rodrigues27@gmail.com>
2025-05-16 19:16:32 -0300

    Tipos primitivos em PHP

--- ex003/index.php
+++ ex003/index.php
@@ -1,20 +1,39 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>Tipos primitivos</title>
 </head>
 
 <body>
-    <h1>Exemplo PHP</h1>
+    <h1>Teste tipos primitivos</h1>
     <?php 
-    date_default_timezone_set("America/Sao_Paulo");
-    print "Hoje é dia " . date("d/M/Y");
-    print " E a hora atual é " . date("G: i: s");
-    ?>
+    // 0x = hexadecimal 0b = binário 0 = 0ctal 
+    // $num = 010;
+    // echo "o valor da variável é $num";
 
+    // $valor = 45.2;
+    // var_dump($valor); // me retorna o valor e o tipo da variavel
+
+    // $num = (int) "123"; // coercao de tipo
+    // var_dump($num);
+
+    // $casado = true;
+    // echo "O valor para casado é $casado"; // retorna 1 ou vazio 
+
+
+    // $vetor = [1, 2.3, "BH", true]; // array
+    // var_dump($vetor);
+
+    class Pessoa {
+        private string $nome;
+    }
+
+    $p = new Pessoa;
+    var_dump($p);
+    ?>
 </body>
 
 </html>
